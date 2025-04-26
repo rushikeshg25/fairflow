5350a4ba145ff0a26d417a41f62b48ae438f4af3
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-04-26 17:15:09 +0530

    feat: company init ins and test

--- programs/fairflow/src/lib.rs
+++ programs/fairflow/src/lib.rs
@@ -3,6 +3,8 @@ use anchor_lang::prelude::*;
 pub mod errors;
 pub mod instructions;
 pub mod states;
+pub use instructions::*;
+pub use states::*;
 
 declare_id!("FZJ5m8nT7mi78VGrGsCGSPRYSK69PS7U2rzvR3CwGcBP");
 
@@ -10,11 +12,14 @@ declare_id!("FZJ5m8nT7mi78VGrGsCGSPRYSK69PS7U2rzvR3CwGcBP");
 pub mod fairflow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_company_state(
+        ctx: Context<Initialize>,
+        name: String,
+        inc_percent: u8,
+        dec_percent: u8,
+        treasury: Pubkey,
+    ) -> Result<()> {
+        ctx.accounts
+            .init_company_state(name, inc_percent, dec_percent, treasury, &ctx.bumps)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
--- tests/fairflow.ts
+++ tests/fairflow.ts
@@ -3,14 +3,33 @@ import { Program } from "@coral-xyz/anchor";
 import { Fairflow } from "../target/types/fairflow";
 
 describe("fairflow", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.fairflow as Program<Fairflow>;
+  const treasuryWallet = anchor.web3.Keypair.generate();
+  const treasury = treasuryWallet.publicKey;
+  const employer = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Company State initialized", async () => {
+    try {
+      const signature = await anchor
+        .getProvider()
+        .connection.requestAirdrop(
+          employer.publicKey,
+          anchor.web3.LAMPORTS_PER_SOL
+        );
+      await anchor.getProvider().connection.confirmTransaction(signature);
+
+      const tx = await program.methods
+        .initializeCompanyState("Test Company", 10, 5, treasury)
+        .signers([employer])
+        .accounts({
+          employer: employer.publicKey,
+          treasury: treasury,
+        })
+        .rpc();
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
   });
 });
